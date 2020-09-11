5219a1471b2fa6356fed4632e17f29ead82d76e0
Jelle Vrieze <jelle.vrieze@gmail.com>
2020-09-11 11:41:08 +0200

    [UPDATE] Shader uses correct fragment position

--- RayCasting/data/shaders/rayTracing.fs
+++ RayCasting/data/shaders/rayTracing.fs
@@ -7,19 +7,16 @@ uniform sampler2D texture;
 
 varying vec4 vertColor;
 varying vec4 vertTexCoord;
+in vec3 fragPosition;
 
 uniform vec2 rayHitPosition;
 uniform vec4 rayHitColor;
 
 void main() 
 {
-    if(texture2D(texture, vertTexCoord.st).a == 0) gl_FragColor = vec4(0,0,0,0);
-    //gl_FragColor = texture2D(texture, vertTexCoord.st) * vertColor;
-    else
-    {
-        vec2 toLight = (rayHitPosition/500)-vertTexCoord.xy;
-        float distToLight = max(length(toLight), 0);
-        vec4 textureColor = texture2D(texture, vertTexCoord.st) * vertColor;
-        gl_FragColor = textureColor * rayHitColor / (distToLight+1);
-    }
+    vec2 toLight = rayHitPosition-fragPosition.xy;
+    float distToLight = max(length(toLight), 0);
+    vec4 distToLightVec = vec4(distToLight, distToLight, distToLight, 1);
+    vec4 textureColor = texture2D(texture, vertTexCoord.st) * vertColor;
+    gl_FragColor = (textureColor * rayHitColor / distToLightVec) * 20;
 }
--- RayCasting/data/shaders/rayTracing.vs
+++ RayCasting/data/shaders/rayTracing.vs
@@ -7,10 +7,12 @@ attribute vec2 texCoord;
 
 varying vec4 vertColor;
 varying vec4 vertTexCoord;
+out vec3 fragPosition;
 
 void main() {
   gl_Position = transform * position;
 
   vertColor = color;
   vertTexCoord = texMatrix * vec4(texCoord, 1.0, 1.0);
+	fragPosition = vec3(texMatrix * vec4(position.xyz, 1));
 }
