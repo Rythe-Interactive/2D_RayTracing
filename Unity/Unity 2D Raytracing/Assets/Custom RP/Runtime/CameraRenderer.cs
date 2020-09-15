4968081f9397fcf66f05ffc66bdd40c12729bfbe
Jelle Vrieze <jelle.vrieze@gmail.com>
2020-09-15 16:06:08 +0200

    [ADD] Custom RP and Expirimental 2D RP

---
+++ Unity/Unity 2D Raytracing/Assets/Custom RP/Runtime/CameraRenderer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CameraRenderer
+{
+    ScriptableRenderContext context;
+    Camera camera;
+
+    public void Render(ScriptableRenderContext context, Camera camera)
+    {
+        this.context = context;
+        this.camera = camera;
+
+        //start rendering stuff
+        CommandBuffer cmd = new CommandBuffer();
+        cmd.ClearRenderTarget(true, true, Color.clear);
+        context.ExecuteCommandBuffer(cmd);
+        context.SetupCameraProperties(camera);
+        cmd.Release();
+
+        context.Submit();
+    }
+}
---
+++ Unity/Unity 2D Raytracing/Assets/Custom RP/Runtime/CustomRenderPipeline.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CustomRenderPipeline : RenderPipeline
+{
+    CameraRenderer camRenderer = new CameraRenderer();
+
+    public CustomRenderPipeline()
+    {
+
+    }
+
+    protected override void Render(ScriptableRenderContext context, Camera[] cameras)
+    {
+        for(int i = 0; i < cameras.Length; ++i)
+        {
+            camRenderer.Render(context, cameras[i]);
+        }
+    }
+}
