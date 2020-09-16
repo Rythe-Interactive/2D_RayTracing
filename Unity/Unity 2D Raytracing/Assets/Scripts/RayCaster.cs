d1edb7aadc19533fc538d5aacd13cc9813fc17d3
Jelle Vrieze <jelle.vrieze@gmail.com>
2020-09-16 11:10:36 +0200

    [ADD] Raycaster setup for unity

---
+++ Unity/Unity 2D Raytracing/Assets/Scripts/RayCaster.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class RayCaster
+{
+    private static List<RayCollider> m_colliders;
+    private static List<RayHit> m_rayHits;
+
+    public static void register(RayCollider collider)
+    {
+        m_colliders.Add(collider);
+    }
+
+    public static void unRegister(RayCollider collider)
+    {
+        m_colliders.Remove(collider);
+    }
+
+    public static void render()
+    {
+        Camera cam = Camera.main;
+
+    }
+}
---
+++ Unity/Unity 2D Raytracing/Assets/Scripts/RayCollider.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class RayCollider : MonoBehaviour
+{
+    public abstract bool Collide(LightRay ray);
+}
